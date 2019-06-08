c3ff0ad2f6c4a405b77f175a230766fe81e066b6
ShinySylveon04 <26980015+ShinySylveon04@users.noreply.github.com>
2019-06-08 14:56:45 -0500

    Added tooltip to trainer description

--- src/views/in-game-player.js
+++ src/views/in-game-player.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { setPlayer } from '../actions';
-import partial from 'lodash/partial';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import { connect } from "react-redux";
+import { setPlayer } from "../actions";
+import partial from "lodash/partial";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import { withStyles } from "@material-ui/core/styles";
 import {
   StyledDropdown,
   createImageDropdownItems,
   createDropdownItems
-} from '../components/Dropdown';
-import { StyledTextField } from '../components/TextField';
-import { ColumnLayout } from '../layouts/column-layout';
-import { passEventValue } from '../utils/pass-event-value';
-import { gen7Messages } from '../utils/gen7-gts-messages';
-import { ORASTrainers } from '../utils/oras-trainers';
-import { getGameGen } from '../utils/get-game-generation';
+} from "../components/Dropdown";
+import { StyledTextField } from "../components/TextField";
+import { ColumnLayout } from "../layouts/column-layout";
+import { passEventValue } from "../utils/pass-event-value";
+import { gen7Messages } from "../utils/gen7-gts-messages";
+import { ORASTrainers } from "../utils/oras-trainers";
+import { getGameGen } from "../utils/get-game-generation";
 
 const mapStateToProps = ({ player }) => player;
 
@@ -27,17 +27,17 @@ const mapDispatchToProps = {
 
 const styles = {
   tooltip: {
-    backgroundColor: '#f5f5f9',
-    color: 'rgba(0, 0, 0, 0.87)',
+    backgroundColor: "#f5f5f9",
+    color: "rgba(0, 0, 0, 0.87)",
     maxWidth: 220,
-    border: '1px solid #dadde9'
+    border: "1px solid #dadde9"
   }
 };
 
 const GTSMessageTextField = ({ setPlayer }) => (
   <StyledTextField
     label="GTS Message"
-    onChange={passEventValue(partial(setPlayer, 'gtsMessage'))}
+    onChange={passEventValue(partial(setPlayer, "gtsMessage"))}
   />
 );
 
@@ -47,7 +47,7 @@ const GTSDropdown = ({ setPlayer, gtsMessage }) => (
     label="GTS Message"
     name="gtsMessage"
     id="gtsMessage"
-    onChange={passEventValue(partial(setPlayer, 'gtsMessage'))}
+    onChange={passEventValue(partial(setPlayer, "gtsMessage"))}
   >
     {createDropdownItems(gen7Messages)}
   </StyledDropdown>
@@ -60,8 +60,26 @@ const GTSMessageInput = ({ game, setPlayer, gtsMessage }) => {
   return <GTSMessageComponent setPlayer={setPlayer} gtsMessage={gtsMessage} />;
 };
 
-const CustomTooltip = ({ classes }) => (
-  <Tooltip placement="bottom" title="test" className={classes.tooltip} />
+const tooltipTitle = (
+  <React.Fragment>
+    <Typography variant="subtitle1">Trainer Description</Typography>
+    <Typography variant="body2">
+      You can find how your trainer appears on the GTS by looking at your
+      Trainer Card or Trainer Passport in-game
+    </Typography>
+  </React.Fragment>
+);
+
+const CustomTooltip = ({ classes, children }) => (
+  <Tooltip
+    disableFocusListener
+    disableTouchListener
+    placement="top"
+    title={tooltipTitle}
+    classes={classes}
+  >
+    {children}
+  </Tooltip>
 );
 
 const StyledTooltip = withStyles(styles)(CustomTooltip);
@@ -71,18 +89,23 @@ const TrainerTextField = ({ setPlayer }) => (
     <StyledTextField
       multiline
       label="Trainer description"
-      onChange={passEventValue(partial(setPlayer, 'trainerDescription'))}
+      onChange={passEventValue(partial(setPlayer, "trainerDescription"))}
     />
     <Typography variant="body2">
+      <StyledTooltip>
+        <IconButton color="primary">
+          <HelpOutlineIcon fontSize="small" />
+        </IconButton>
+      </StyledTooltip>
       Describe how your trainer looks in-game
-      <StyledTooltip />
-      <IconButton color="primary">
-        <HelpOutlineIcon fontSize="small" />
-      </IconButton>
     </Typography>
-    <Typography variant="caption">
-      <a href="https://www.serebii.net/xy/customisation.shtml">
-        XY Clothing List
+    <Typography variant="body2">
+      Clothing Lists:{" "}
+      <a href="https://www.serebii.net/xy/customisation.shtml">XY</a> |{" "}
+      <a href="https://www.serebii.net/sunmoon/customisation.shtml">Sun/Moon</a>{" "}
+      |{" "}
+      <a href="https://www.serebii.net/ultrasunultramoon/customisation.shtml">
+        Ultra Sun/Ultra Moon
       </a>
     </Typography>
   </React.Fragment>
@@ -95,7 +118,7 @@ const TrainerDropdown = ({ setPlayer, value }) => (
       label="Trainer Description"
       name="trainerDescription"
       id="trainerDescription"
-      onChange={passEventValue(partial(setPlayer, 'trainerDescription'))}
+      onChange={passEventValue(partial(setPlayer, "trainerDescription"))}
     >
       {createImageDropdownItems(ORASTrainers)}
     </StyledDropdown>
@@ -104,7 +127,7 @@ const TrainerDropdown = ({ setPlayer, value }) => (
 
 const TrainerDescriptionInput = ({ game, setPlayer, value }) => {
   const TrainerDescription =
-    game === 'ORAS' ? TrainerDropdown : TrainerTextField;
+    game === "ORAS" ? TrainerDropdown : TrainerTextField;
 
   return <TrainerDescription setPlayer={setPlayer} value={value} />;
 };
@@ -126,17 +149,17 @@ const InGamePlayerView = ({
           label="Game Language"
           name="gameLanguage"
           id="gameLanguage"
-          onChange={passEventValue(partial(setPlayer, 'language'))}
+          onChange={passEventValue(partial(setPlayer, "language"))}
         >
           {createDropdownItems([
-            'English',
-            'Japanese',
-            'French',
-            'Italian',
-            'German',
-            'Spanish',
-            'Korean',
-            'Chinese'
+            "English",
+            "Japanese",
+            "French",
+            "Italian",
+            "German",
+            "Spanish",
+            "Korean",
+            "Chinese"
           ])}
         </StyledDropdown>
         <TrainerDescriptionInput
@@ -146,7 +169,7 @@ const InGamePlayerView = ({
         />
         <StyledTextField
           label="In game name"
-          onChange={passEventValue(partial(setPlayer, 'inGameName'))}
+          onChange={passEventValue(partial(setPlayer, "inGameName"))}
         />
         <GTSMessageInput
           game={game}
@@ -155,7 +178,7 @@ const InGamePlayerView = ({
         />
         <StyledTextField
           label="3DS Region"
-          onChange={passEventValue(partial(setPlayer, 'consoleRegion'))}
+          onChange={passEventValue(partial(setPlayer, "consoleRegion"))}
         />
       </ColumnLayout>
       {children}
